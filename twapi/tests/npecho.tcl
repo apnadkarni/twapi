10bc1fbecacc834fdc829992b9738292cf3932e1
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-08-05 08:26:20 +0530

    Moved named pipe echo servers to separate file

---
+++ twapi/tests/npecho.tcl
@@ -0,0 +1,107 @@
+# Named pipe test routines
+
+source [file join [file dirname [info script]] testutil.tcl]
+
+load_twapi
+
+proc np_echo_usage {} {
+    puts stderr {
+Usage:
+     tclsh npecho.tcl syncserver ?PIPENAME? ?TIMEOUT?
+        -- runs the synchronous echo server
+     tclsh npecho.tcl asyncserver ?PIPENAME? ?TIMEOUT?
+        -- runs the asynchronous echo server
+    }
+    exit 1
+}
+
+proc np_echo_server_sync_accept {chan} {
+    set ::np_echo_server_status connected
+    fileevent $chan writable {}
+}
+
+proc np_echo_server_sync {{name {\\.\pipe\twapiecho}} {timeout 20000}} {
+    set timer [after $timeout "set ::np_echo_server_status timeout"]
+    set echo_fd [::twapi::namedpipe_server $name]
+    fconfigure $echo_fd -buffering line -translation crlf -eofchar {} -encoding utf-8
+    fileevent $echo_fd writable [list ::np_echo_server_sync_accept $echo_fd]
+    vwait ::np_echo_server_status
+    after cancel $timer
+    set msgs 0
+    set last_size 0
+    set total 0
+    if {$::np_echo_server_status eq "connected"} {
+        while {1} {
+            if {[gets $echo_fd line] >= 0} {
+                puts $echo_fd $line
+                if {$line eq "exit"} {
+                    break
+                }
+                incr msgs
+                set last_size [string length $line]
+                incr total $last_size
+            } else {
+                puts stderr "Unexpected eof from echo client"
+                break
+            }
+        }
+    } else {
+        puts stderr "echo_server: $::np_echo_server_status"
+    }
+    close $echo_fd
+    return [list $msgs $total $last_size]
+}
+
+proc np_echo_client {args} {
+    array set opts [twapi::parseargs args {
+        {name.arg {\\.\pipe\twapiecho}}
+        {density.int 2}
+        {limit.int 10000}
+    }]
+
+    set msgs 0
+    set last 0
+    set fd [twapi::namedpipe_client $opts(name)]
+    fconfigure $fd -buffering line -translation crlf -eofchar {} -encoding utf-8
+    for {set i 1} {$i < 100000} {incr i [expr {1+($i+1)/$opts(density)}]} {
+        set request [string repeat X $i]
+        puts $fd $request
+        set response [gets $fd]
+        if {$request ne $response} {
+            puts "Mismatch in message of size $i"
+        }
+        incr msgs
+        set last $i
+    }
+    puts $fd "exit"
+    close $fd
+    puts "Messages: $msgs, Last: $last"
+}
+
+
+
+# Main code
+
+if {[string equal -nocase [file normalize $argv0] [file normalize [info script]]]} {
+
+    # We are being directly sourced, not as a library
+    if {[llength $argv] == 0} {
+        np_echo_usage
+    }
+
+    switch -exact -- [lindex $argv 0] {
+        syncserver {
+            puts "Running syncserver"
+            foreach {nmsgs nbytes last} [eval np_echo_server_sync [lrange $argv 1 end]] break
+        }
+        asyncserver {
+            puts "Running asyncserver"
+            foreach {nmsgs nbytes last} [eval np_echo_server_async [lrange $argv 1 end]] break
+        }
+        default {
+            np_echo_usage
+        }
+    }
+
+    puts "Received $nmsgs lines with $nbytes characters. Last message $last characters."
+}
