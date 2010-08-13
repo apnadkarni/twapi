13aab86c8896d4fd6e309c602bc102bae2e68cf0
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-08-13 10:01:03 +0530

    More tweaking of builds and distributions

--- twapi/buildall.cmd
+++ twapi/buildall.cmd
@@ -1,10 +1,18 @@
-rem Builds all configurations of TWAPI
-nmake %*
-nmake distribution %*
-nmake tmdistribution  %*
-nmake SERVERONLY=1  %*
-nmake distribution SERVERONLY=1  %*
-nmake tmdistribution SERVERONLY=1  %*
-nmake DESKTOPONLY=1  %*
-nmake distribution DESKTOPONLY=1  %*
-nmake tmdistribution DESKTOPONLY=1  %*
+rem Builds all release configurations of TWAPI
+
+rmdir/s/q temp
+
+nmake
+
+rem Traditional distribution
+nmake /s /nologo /a
+nmake /s /nologo /a distribution
+
+rem Tcl 8.5 module
+nmake /s /nologo /a EMBED_SCRIPT=lzma
+nmake /s /nologo /a EMBED_SCRIPT=lzma tmdistribution
+
+rem Tcl single file DLL
+nmake /s /nologo /a EMBED_SCRIPT=lzma
+nmake /s /nologo /a EMBED_SCRIPT=lzma dlldistribution
+
