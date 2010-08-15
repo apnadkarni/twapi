0abdbc5d3fe5978b78927b76a64312779ba3a0b3
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-08-15 13:57:15 +0530

    Reworked some resource interfaces

--- twapi/tcl/resource.tcl
+++ twapi/tcl/resource.tcl
@@ -14,22 +14,17 @@ proc twapi::begin_resource_update {path args} {
     return [BeginUpdateResource $path $opts(deleteall)]
 }
 
+# Note this is not an alias because we want to control arguments
+# to UpdateResource (which can take more args that specified here)
 proc twapi::delete_resource {hmod restype resname langid} {
-    # String resources have to be handled differently
-    if {$restype != 6} {
-        UpdateResource $hmod $restype $resname $langid
-    } else {
-        TBD - locate string resource and delete it
-    }
+    UpdateResource $hmod $restype $resname $langid
 }
 
+
+# Note this is not an alias because we want to make sure $bindata is specified
+# as an argument.
 proc twapi::update_resource {hmod restype resname langid bindata} {
-    # String resources have to be handled differently
-    if {$restype != 6} {
-        UpdateResource $hmod $restype $resname $langid $bindata
-    } else {
-        TBD - locate string resource and update it
-    }
+    UpdateResource $hmod $restype $resname $langid $bindata
 }
 
 proc twapi::end_resource_update {hmod args} {
@@ -41,11 +36,10 @@ proc twapi::end_resource_update {hmod args} {
 }
 
 proc twapi::read_resource {hmod restype resname langid} {
-    # Strings (type 6) have to be handled differently as they are stored blocks
-    if {$restype != 6} {
-        return [Twapi_LoadResource $hmod [FindResourceEx $hmod $restype $resname $langid]]
-    }
+    return [Twapi_LoadResource $hmod [FindResourceEx $hmod $restype $resname $langid]]
+}
 
+proc twapi::read_resource_string {hmod resname langid} {
     # As an aside, note that we do not use a LoadString call
     # because it does not allow for specification of a langid
     
@@ -57,8 +51,38 @@ proc twapi::read_resource {hmod restype resname langid} {
         error "String resources must have an integer id"
     }
 
-    # Strings are stored in blocks of 16, so figure out the block id
-    set block_id [expr {($resname / 16) + 1}]
-    set block [Twapi_LoadResource $hmod [FindResourceEx $hmod 6 $block_id $langid]]
-    return [lindex [Twapi_SplitStringResource $block] [expr {$resname & 15}]]
+    foreach {block_id index_within_block} [resource_stringid_to_stringblockid $resname] break
+
+    return [lindex \
+                [resource_stringblock_to_strings \
+                     [read_resource $hmod 6 $block_id $langid] ] \
+                $index_within_block]
 }
+
+interp alias {} twapi::resource_stringblock_to_strings {} twapi::Twapi_SplitStringResource
+
+# Give a list of strings, formats it as a string block. Number of strings
+# must not be greater than 16. If less than 16 strings, remaining are
+# treated as empty.
+proc twapi::strings_to_resource_stringblock {strings} {
+    if {[llength $strings] > 16} {
+        error "Cannot have more than 16 strings in a resource string block."
+    }
+
+    for {set i 0} {$i < 16} {incr i} {
+        set s [lindex $strings $i]
+        set n [string length $s]
+        append bin [binary format ta* $n [encoding convertto unicode $s]]
+    }
+
+    return $bin
+}
+
+proc twapi::resource_stringid_to_stringblockid {id} {
+    # Strings are stored in blocks of 16, with block id's beginning at 1, not 0
+    return [list [expr {($id / 16) + 1}] [expr {$id & 15}]]
+}
+
+interp alias {} twapi::enumerate_resource_languages {} twapi::Twapi_EnumResourceLanguages
+interp alias {} twapi::enumerate_resource_names {} twapi::Twapi_EnumResourceNames
+interp alias {} twapi::enumerate_resource_types {} twapi::Twapi_EnumResourceTypes
