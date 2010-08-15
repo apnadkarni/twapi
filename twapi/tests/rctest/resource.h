9f5eb03a32c1694f325ad6af04678fa317293b70
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-08-15 22:08:28 +0530

    Added rctest dll for testing resource management commands

---
+++ twapi/tests/rctest/resource.h
@@ -0,0 +1,86 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by rctest.rc
+//
+#define IDR_ACCELERATOR1                101
+#define IDD_FORMVIEW                    101
+#define IDI_ICON1                       102
+#define IDC_CURSOR1                     103
+#define IDC_CURSOR2                     104
+#define IDR_HTML1                       105
+#define IDB_BITMAP1                     106
+#define IDD_PROPPAGE_MEDIUM             106
+#define IDI_ICON2                       107
+#define IDI_ICON3                       108
+#define IDD_DIALOG1                     109
+#define IDR_MENU1                       110
+#define IDS_STRING1                     58117
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
+
+#define IDS_NEUTRAL                     1
+#define IDS_NEUTRAL_DEFAULT             2
+#define IDS_NEUTRAL_SYSDEFAULT          3
+#define IDS_BULGARIAN                   4
+#define IDS_CROATIAN                    5
+#define IDS_CZECH                       6
+#define IDS_DANISH                      7
+#define IDS_DUTCH_NETHERLANDS           8
+#define IDS_DUTCH_BELGIUM               9
+#define IDS_ENGLISH_US                  10
+#define IDS_ENGLISH_UK                  11
+#define IDS_ENGLISH_AUSTRALIA           12
+#define IDS_ENGLISH_CANADA              13
+#define IDS_ENGLISH_NEWZEALAND          14
+#define IDS_ENGLISH_IRELAND             15
+#define IDS_FINNISH                     16
+#define IDS_FRENCH_FRANCE               17
+#define IDS_FRENCH_BELGIUM              18
+#define IDS_FRENCH_CANADA               19
+#define IDS_FRENCH_SWITZERLAND          20
+#define IDS_GERMAN_SWITZERLAND          21
+#define IDS_GERMAN_AUSTRIA              22
+#define IDS_GREEK                       23
+#define IDS_HUNGARIAN                   24
+#define IDS_ICELANDIC                   25
+#define IDS_ITALIAN_ITALY               26
+#define IDS_ITALIAN_SWITZERLAND         27
+#define IDS_JAPANESE                    28
+#define IDS_KOREAN                      29
+#define IDS_NORWEGIAN_BOKMAL            30
+#define IDS_NORWEGIAN_NYNORSK           31
+#define IDS_POLISH                      32
+#define IDS_PORTUGUESE_BRAZIL           33
+#define IDS_PORTUGUESE_PORTUGAL         34
+#define IDS_ROMANIAN                    35
+#define IDS_RUSSIAN                     36
+#define IDS_SLOVAK                      37
+#define IDS_SLOVENIAN                   38
+#define IDS_SWEDISH                     39
+#define IDS_TURKISH                     40
+#define IDS_GERMAN_GERMANY              41
+#define IDS_INVALIDAPPNAME              42
+#define IDS_INVALIDLANGUAGE             43
+#define IDS_INVALIDBLOCKID              44
+#define IDS_BLOCKCREATIONFAILED         45
+#define IDS_STRIDCOLUMN                 46
+#define IDS_STRTEXTCOLUMN               47
+#define IDS_STRINVALIDBLOCK             48
+#define IDS_UPDATENOTIMPLEMENTED        49
+#define IDS_UPDATEFAILED                50
+#define IDS_NOMEMORY                    51
+#define IDS_OPERATIONOK                 52
+#define IDS_APPLOADFAIL                 53
+#define IDS_RESNOTFOUND                 54
+#define IDS_RESLOADFAILED               55
+#define IDS_UKNOWNERR                   56
+#define IDS_UPDATESUCCEEDED             57
