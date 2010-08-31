8f237b0e5995b2f53d1abfe62207addf1e19dc10
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2010-08-31 15:49:41 +0530

    Automated entire build to build both 64 and 32 in one go

--- twapi/buildall.cmd
+++ twapi/buildall.cmd
@@ -1,17 +1,36 @@
 rem Builds all release configurations of TWAPI
 
+echo BUILDALL ENTER %CPU%
+
+nmake /s /nologo /a clean
+nmake /s /nologo /a EMBED_SCRIPT=lzma clean
+
+rem Do not remove existing distributions if doing 64 bit build
+rem and do 64-bit build first so dll can be included in full distro
+IF "x%CPU%" == "xAMD64" goto dobuild
+echo REMOVE TEMP %CPU%
 rmdir/s/q temp
+echo REMOVE TEMP %CPU%
+cmd /c buildall64.cmd
 
-rem Traditional distribution
+:dobuild
+
+echo DOING BUILD %CPU%
+rem Doing build
 nmake /s /nologo /a
-nmake /s /nologo /a distribution
-
-rem LZMA distributions
+echo DOING BUILD LZMA %CPU%
 nmake /s /nologo /a EMBED_SCRIPT=lzma
-rem DLL based package
+
+rem Skip distribution in 64bit build - included in 32-bit builds
+IF "x%CPU%" == "xAMD64" goto singlefile
+echo DOING DISTRIBUTION %CPU%
 nmake /s /nologo /a distribution
-rem Tcl 8.5 module
+echo DOING DISTRIBUTION LZMA %CPU%
+nmake /s /nologo /a EMBED_SCRIPT=lzma distribution
+
+:singlefile
+echo DOING TM %CPU%
 nmake /s /nologo /a EMBED_SCRIPT=lzma tmdistribution
-rem Tcl single file DLL
+echo DOING DLL %CPU%
 nmake /s /nologo /a EMBED_SCRIPT=lzma dlldistribution
 
