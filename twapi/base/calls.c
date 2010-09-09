13faa91b4d83e34f8b087b37ccfb16c208984b83
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-09-09 17:01:02 +0530

    Bugfix: Bogus data being returned in get_tcp/udp_connections because level param to Twapi_FormatExtended*Table was being parsed as bool instead of int.

--- twapi/base/calls.c
+++ twapi/base/calls.c
@@ -1583,7 +1583,7 @@ int Twapi_CallObjCmd(TwapiInterpContext *ticP, Tcl_Interp *interp, int objc, Tcl
         case 10000: // Twapi_FormatExtendedTcpTable
         case 10001: // Twapi_FormatExtendedUdpTable
             if (TwapiGetArgs(interp, objc-2, objv+2,
-                             GETVOIDP(pv), GETINT(dw), GETBOOL(dw2),
+                             GETVOIDP(pv), GETINT(dw), GETINT(dw2),
                              ARGEND) != TCL_OK)
                 return TCL_ERROR;
             return (func == 10000 ? Twapi_FormatExtendedTcpTable : Twapi_FormatExtendedUdpTable)
