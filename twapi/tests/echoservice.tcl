85f84fbec54bedface9769962ea2c41f0e2f4a07
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-11-23 16:47:02 +0530

    Added test for run_as_service

--- twapi/tests/echoservice.tcl
+++ twapi/tests/echoservice.tcl
@@ -15,7 +15,14 @@ manager GUI.
     exit 1
 }
 
-package require twapi
+# If in source dir, we load that twapi in preference to installed package
+set echo_script_dir [file dirname [file dirname [file normalize [info script]]]]
+if {[file exists [file join $echo_script_dir tcl twapi.tcl]]} {
+    uplevel #0 source [file join $echo_script_dir tcl twapi.tcl]
+} else {
+    uplevel #0 package require twapi
+}
+
 
 ################################################################
 # The echo_server code is almost verbatim from the Tcl Developers
