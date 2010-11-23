d25be3c4b6f380082c3049bd7d2c934fae97ee55
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2010-11-23 22:37:00 +0530

    Script to copy required files to test system

---
+++ twapi/tests/setuptarget.tcl
@@ -0,0 +1,31 @@
+# Copies the test environment to target 
+# tclsh setuptestenv.tcl TARGETPATH
+
+if {[llength $argv] != 1} {
+    puts stderr "Usage: [info nameofexecutable] [info script] TARGETPATH"
+}
+
+set target [file normalize [lindex $argv 0]]
+if {[file exists $target]} {
+    puts stderr "Error: Target path $target already exists. Will not overwrite."
+    exit 1
+}
+
+file mkdir [set target_bindir [file join $target bin]]
+file mkdir [set target_libdir [file join $target lib]]
+
+set tcl_root [file dirname [file dirname [file normalize [info nameofexecutable]]]]
+foreach fpat {*.dll *.exe} {
+    foreach fn [glob [file join $tcl_root bin $fpat]] {
+        file copy $fn $target_bindir
+    }
+}
+
+foreach dirpat {dde* reg* tcl8* tk*} {
+    foreach dir [glob [file join $tcl_root lib $fpat]] {
+        file copy $dir $target_libdir
+    }
+}
+
+# Finally copy the testscripts
+file copy [file dirname [file normalize [info script]]] [file join $target tests]
