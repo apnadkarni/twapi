5c7e347c7848634b7af55dc2d33f1abc17175404
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2011-01-12 23:34:51 +0530

    Console tests

--- twapi/tests/testaudit.tcl
+++ twapi/tests/testaudit.tcl
@@ -52,6 +52,8 @@ array set do_not_test {
     get_process_handle        get_process_info
     get_connected_shares      deprecated_alias
     kl_get_default            deprecated_alias
+    set_console_output_mode   deprecated_alias
+    set_console_input_mode   deprecated_alias
 }
 set missing {}
 foreach cmd [twapi::_get_public_procs] {
