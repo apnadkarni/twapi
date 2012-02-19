9fe23b1c945dcae699ccb05e00569507d2003ce7
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2012-02-19 23:06:49 +0530

    Added eventlog module

--- twapi/service/twapi_service.h
+++ twapi/service/twapi_service.h
@@ -1,6 +1,8 @@
 #ifndef TWAPI_SERVICE_H
 #define TWAPI_SERVICE_H
 
+#include <winsvc.h>
+
 int Twapi_BecomeAService(TwapiInterpContext *, int objc, Tcl_Obj *CONST objv[]);
 
 int Twapi_SetServiceStatus(TwapiInterpContext *, int objc, Tcl_Obj *CONST objv[]);
