43a4f9a0706c9ef6d0785c254d3f193d897aeda3
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2012-02-23 14:50:39 +0530

    Subsume hotkey.c into input.c

--- twapi/input/input.c
+++ twapi/input/input.c
@@ -21,6 +21,40 @@ int Twapi_BlockInput(Tcl_Interp *interp, BOOL block);
 int Twapi_SendUnicode(TwapiInterpContext *ticP, Tcl_Obj *input_obj);
 int Twapi_SendInput(TwapiInterpContext *ticP, Tcl_Obj *input_obj);
 
+/* TBD - move to script level code */
+int Twapi_RegisterHotKey(TwapiInterpContext *ticP, int id, UINT modifiers, UINT vk)
+{
+    HWND hwnd;
+
+    // Get the common notification window.
+    hwnd = Twapi_GetNotificationWindow(ticP);
+    if (hwnd == NULL)
+        return TCL_ERROR;
+
+    if (RegisterHotKey(hwnd, id, modifiers, vk))
+        return TCL_OK;
+    else
+        return TwapiReturnSystemError(ticP->interp);
+}
+
+
+int Twapi_UnregisterHotKey(TwapiInterpContext *ticP, int id)
+{
+    // Note since we are using the common window for notifications,
+    // we do not destroy it. Just unregister the hot key.
+    HWND hwnd;
+    hwnd = Twapi_GetNotificationWindow(ticP);
+    
+    if (UnregisterHotKey(hwnd, id))
+        return TCL_OK;
+    else
+        return TwapiReturnSystemError(ticP->interp);
+}
+
+
+
+
+
 int Twapi_BlockInput(Tcl_Interp *interp, BOOL block)
 {
     BOOL result = BlockInput(block);
