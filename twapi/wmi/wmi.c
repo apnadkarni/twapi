7d4fdae9ee617580c004b9b9cedf93810d368b39
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2012-03-05 22:40:41 +0530

    Made wmi module

--- twapi/wmi/wmi.c
+++ twapi/wmi/wmi.c
@@ -8,6 +8,10 @@
 #include "twapi.h"
 #include <wbemidl.h>
 
+#ifndef TWAPI_SINGLE_MODULE
+static HMODULE gModuleHandle;     /* DLL handle to ourselves */
+#endif
+
 TCL_RESULT Twapi_IMofCompiler_CompileFileOrBuffer(TwapiInterpContext *ticP, int type, int objc, Tcl_Obj *CONST objv[])
 {
     IMofCompiler *ifc;
@@ -87,4 +91,71 @@ TCL_RESULT Twapi_IMofCompiler_CompileFileOrBuffer(TwapiInterpContext *ticP, int 
         return Twapi_AppendSystemError(ticP->interp, wcsi.hRes ? wcsi.hRes : hr);
     }
 }                     
+
+static int Twapi_WmiCallObjCmd(TwapiInterpContext *ticP, Tcl_Interp *interp, int objc, Tcl_Obj *CONST objv[])
+{
+    int func;
+
+    if (objc < 2)
+        return TwapiReturnError(interp, TWAPI_BAD_ARG_COUNT);
+    CHECK_INTEGER_OBJ(interp, func, objv[1]);
+
+    switch (func) {
+    case 1: // IMofCompiler_CompileBuffer
+        return Twapi_IMofCompiler_CompileFileOrBuffer(ticP, 0, objc-2, objv+2);
+    case 2: // IMofCompiler_CompileFile
+        return Twapi_IMofCompiler_CompileFileOrBuffer(ticP, 1, objc-2, objv+2);
+    case 3: // IMofCompiler_CreateBMOF
+        return Twapi_IMofCompiler_CompileFileOrBuffer(ticP, 2, objc-2, objv+2);
+    }
+
+    return TwapiReturnError(interp, TWAPI_INVALID_FUNCTION_CODE);
+}
+
+
+static int Twapi_WmiInitCalls(Tcl_Interp *interp, TwapiInterpContext *ticP)
+{
+    /* Create the underlying call dispatch commands */
+    Tcl_CreateObjCommand(interp, "twapi::WmiCall", Twapi_WmiCallObjCmd, ticP, NULL);
+
+    /* Now add in the aliases for the Win32 calls pointing to the dispatcher */
+#define CALL_(fn_, code_)                                         \
+    do {                                                                \
+        Twapi_MakeCallAlias(interp, "twapi::" #fn_, "twapi::WmiCall", # code_); \
+    } while (0);
+
+    CALL_(IMofCompiler_CompileBuffer, 1); // Tcl
+    CALL_(IMofCompiler_CompileFile, 2); // Tcl
+    CALL_(IMofCompiler_CreateBMOF, 3); // Tcl
+
+#undef CALL_
+
+    return TCL_OK;
+}
+
+
+#ifndef TWAPI_SINGLE_MODULE
+BOOL WINAPI DllMain(HINSTANCE hmod, DWORD reason, PVOID unused)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+        gModuleHandle = hmod;
+    return TRUE;
+}
+#endif
+
+/* Main entry point */
+#ifndef TWAPI_SINGLE_MODULE
+__declspec(dllexport) 
+#endif
+int Twapi_Wmi_Init(Tcl_Interp *interp)
+{
+    /* IMPORTANT */
+    /* MUST BE FIRST CALL as it initializes Tcl stubs */
+    if (Tcl_InitStubs(interp, TCL_VERSION, 0) == NULL) {
+        return TCL_ERROR;
+    }
+
+    return Twapi_ModuleInit(interp, WLITERAL(MODULENAME), MODULE_HANDLE,
+                            Twapi_WmiInitCalls, NULL) ? TCL_OK : TCL_ERROR;
+}
 
