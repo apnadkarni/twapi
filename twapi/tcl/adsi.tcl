4bff8adf7b52859ece4cc7acd3341c6b65985e2a
Ashok P. Nadkarni <apnadkarni@users.sourceforge.net>
2012-03-17 07:00:02 +0530

    Synch module and some minor cleanup

--- twapi/tcl/adsi.tcl
+++ twapi/tcl/adsi.tcl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2010, Ashok P. Nadkarni
+# Copyright (c) 2010-2012, Ashok P. Nadkarni
 # All rights reserved.
 #
 # See the file LICENSE for license
@@ -8,40 +8,20 @@
 
 # TBD - document
 proc twapi::adsi_translate_name {name to {from 0}} {
+    set map {
+        unknown 0 fqdn 1 samcompatible 2 display 3 uniqueid 6
+        canonical 7 userprincipal 8 canonicalex 9 serviceprincipal 10
+        dnsdomain 12
+    }
     if {! [string is integer -strict $to]} {
-        switch -exact -- $to {
-            fqdn          { set to 1 }
-            samcompatible { set to 2 }
-            display       { set to 3 }
-            uniqueid      { set to 6 }
-            canonical     { set to 7 }
-            userprincipal { set to 8 }
-            canonicalex   { set to 9 }
-            serviceprincipal {set to 10 }
-            dnsdomain     { set to 12 }
-            unknown -
-            default {
-                error "Invalid target format specifier '$to'"
-            }
+        set to [dict get $map $to]
+        if {$to == 0} {
+            error "'unknown' is not a valid target format."
         }
     }
 
     if {! [string is integer -strict $from]} {
-        switch -exact -- $from {
-            unknown       { set from 0 }
-            fqdn          { set from 1 }
-            samcompatible { set from 2 }
-            display       { set from 3 }
-            uniqueid      { set from 6 }
-            canonical     { set from 7 }
-            userprincipal { set from 8 }
-            canonicalex   { set from 9 }
-            serviceprincipal {set from 10 }
-            dnsdomain     { set from 12 }
-            default {
-                error "Invalid source format specifier '$from'"
-            }
-        }
+        set from [dict get $map $from]
     }
         
     return [TranslateName $name $from $to]
