1bdb1103c425dcf551f9d92520483a3f2f484900
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2012-07-11 13:04:37 +0530

    Some more evt stuff

--- twapi/tcl/evt.tcl
+++ twapi/tcl/evt.tcl
@@ -8,6 +8,33 @@
 
 namespace eval twapi {}
 
+proc twapi::evt_open_session {server args} {
+    array set opts [parseargs args {
+        user.arg
+        domain.arg
+        {password.arg ""}
+        {authtype.arg 0}
+    } -maxleftover 0]
+
+    if {![string is integer -strict $opts(authtype)]} {
+        set opts(authtype) [dict get {default 0 negotiate 1 kerberos 2 ntlm 3} [string tolower $opts(authtype)]]
+    }
+
+    if {![info exists opts(user)]} {
+        set opts(user) $::env(USERNAME)
+    }
+
+    if {![info exists opts(domain)]} {
+        set opts(domain) $::env(USERDOMAIN)
+    }
+
+    return [EvtOpenSession 1 [list $server $opts(user) $opts(domain) $opts(password) $opts(authtype)] 0 0]
+}
+
+proc twapi::evt_close {hevt} {
+    return [EvtClose $hevt]
+}
+
 proc twapi::evt_channels {{hevtsess NULL}} {
     set chnames {}
     set hevt [EvtOpenChannelEnum $hevtsess 0]
