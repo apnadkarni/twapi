ac33116a13514d526aa97809f5d3f0990aa92cd8
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2013-09-02 19:39:49 +0530

    Refactored InitializeSecurityContext and AcceptSecurityContext

--- twapi/crypto/twapi_crypto.h
+++ twapi/crypto/twapi_crypto.h
@@ -10,5 +10,9 @@ int TwapiSspiInitCalls(Tcl_Interp *interp, TwapiInterpContext *ticP);
 #include <wdigest.h>            /* For VC6 */
 #endif
 
+void TwapiRegisterCertPointer(Tcl_Interp *, PCCERT_CONTEXT);
+void TwapiRegisterCertPointerTic(TwapiInterpContext *, PCCERT_CONTEXT );
+TCL_RESULT TwapiUnregisterCertPointer(Tcl_Interp *, PCCERT_CONTEXT);
+TCL_RESULT TwapiUnregisterCertPointerTic(TwapiInterpContext *, PCCERT_CONTEXT);
 
 #endif
