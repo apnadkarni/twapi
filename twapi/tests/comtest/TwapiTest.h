dfc8c4b259dd433c3d4099de539c5d5e79ff71d8
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2013-12-10 09:43:03 +0530

    Fix JMPSetDataVector proto

--- twapi/tests/comtest/TwapiTest.h
+++ twapi/tests/comtest/TwapiTest.h
@@ -41,7 +41,7 @@ END_COM_MAP()
 
 // ITwapiTest
 public:
-	STDMETHOD(JMPSetDataVector)(/*[in]*/ VARIANT vec, /*[out, retval]*/ BOOL *pBool);
+	STDMETHOD(JMPSetDataVector)(/*[in]*/ VARIANT vec, /*[out, retval]*/ int *pVt);
 	STDMETHOD(OpenDoc6)(BSTR FileName,long Type,long Options, 
 	     BSTR Configuration, long* Errors, long* Warnings, IDispatch** Retval);
 	STDMETHOD(GetApplicationNames)(/*[in, out]*/ VARIANT *varPP);
