9e3a9e9b4bf449032b37882bdf6fe60279747062
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-01-29 22:14:07 +0530

    Added PdhGetFormattedCounterArray

--- twapi/pdh/pdh.c
+++ twapi/pdh/pdh.c
@@ -16,7 +16,6 @@ static HMODULE gModuleHandle;     /* DLL handle to ourselves */
 /* Define interfaces to the PDH performance monitoring library */
 
 /* PDH */
-void TwapiPdhRestoreLocale(void);
 int Twapi_PdhParseCounterPath(TwapiInterpContext *, LPCWSTR buf, DWORD dwFlags);
 int Twapi_PdhGetFormattedCounterValue(Tcl_Interp *, HANDLE hCtr, DWORD fmt);
 int Twapi_PdhLookupPerfNameByIndex(Tcl_Interp *,  LPCWSTR machine, DWORD ctr);
@@ -31,7 +30,7 @@ int Twapi_PdhEnumObjectItems(TwapiInterpContext *,
 
 #if (TCL_MAJOR_VERSION == 8 && TCL_MINOR_VERSION < 5)
 /* Reset locale back to C if necessary. */
-void TwapiPdhRestoreLocale()
+static void TwapiPdhRestoreLocale()
 {
     /*
      * PDH calls do a setlocale which causes Tcl's expression parsing to
@@ -273,6 +272,82 @@ int Twapi_PdhParseCounterPath(
     return result;
 }
 
+static Tcl_Obj *ObjFromPDH_FMT_COUNTERVALUE(Tcl_Interp *interp, PDH_FMT_COUNTERVALUE *valP, DWORD dwFormat)
+{
+    if (valP->CStatus == ERROR_SUCCESS) {
+        switch (dwFormat & (PDH_FMT_LARGE | PDH_FMT_DOUBLE | PDH_FMT_LONG)) {
+        case PDH_FMT_LONG:   return ObjFromLong(valP->longValue);
+        case PDH_FMT_LARGE:  return ObjFromWideInt(valP->largeValue);
+        case PDH_FMT_DOUBLE: return Tcl_NewDoubleObj(valP->doubleValue);
+        default:
+            ObjSetResult(interp,
+                         Tcl_ObjPrintf("Invalid PDH counter format value 0x%x",
+                                       dwFormat));
+            return NULL;
+        }
+    }
+    Twapi_AppendSystemErrorEx(interp, valP->CStatus, Tcl_ObjPrintf("Counter value status failure %d", valP->CStatus));
+    return NULL;
+}
+
+static Tcl_Obj *ObjFromPDH_COUNTER_VALUE_ITEM(Tcl_Interp *interp, PDH_FMT_COUNTERVALUE_ITEM_W *itemP, DWORD dwFormat)
+{
+    Tcl_Obj *objs[2];
+    objs[1] = ObjFromPDH_FMT_COUNTERVALUE(interp, &itemP->FmtValue, dwFormat);
+    if (objs[1] == NULL)
+        return NULL;            /* interp already holds error */
+        
+    objs[0] = ObjFromUnicode(itemP->szName);
+    return ObjNewList(ARRAYSIZE(objs), objs);
+}
+
+TCL_RESULT Twapi_PdhGetFormattedCounterArray(
+    Tcl_Interp *interp,
+    HANDLE hCounter,
+    DWORD dwFormat
+    )
+{
+    PDH_STATUS pdh_status;
+    DWORD sz, nitems;
+    PDH_FMT_COUNTERVALUE_ITEM_W *itemP;
+    TCL_RESULT res;
+    int i;
+    MemLifo *memlifoP;
+    MemLifoMarkHandle mark;
+
+    memlifoP = TwapiMemLifo();
+    mark = MemLifoPushMark(memlifoP);
+    
+    /* Always get required size first since docs say do not rely on
+     * returned size for allocation if it was not passed in as 0.
+     */
+    sz = 0;
+    pdh_status = PdhGetFormattedCounterArrayW(hCounter, dwFormat, &sz, &nitems, NULL);
+    /* Number of items might change so try a few times in a loop */
+    for (i = 0; i < 10 && pdh_status == PDH_MORE_DATA; ++i) {
+        itemP = MemLifoPushFrame(memlifoP, sz, NULL);
+        pdh_status = PdhGetFormattedCounterArrayW(hCounter, dwFormat, &sz, &nitems, itemP);
+    }
+    if (pdh_status != ERROR_SUCCESS)
+        res = Twapi_AppendSystemError(interp, pdh_status);
+    else {
+        Tcl_Obj **itemObjs = MemLifoAlloc(memlifoP, nitems * sizeof(*itemObjs), NULL);
+        res = TCL_OK;
+        for (i = 0; i < nitems; ++i) {
+            itemObjs[i] = ObjFromPDH_COUNTER_VALUE_ITEM(interp, &itemP[i], dwFormat);
+            if (itemObjs[i] == NULL) {
+                ObjDecrArrayRefs(i, itemObjs);
+                res = TCL_ERROR;
+                break;
+            }
+        }
+        if (res == TCL_OK)
+            ObjSetResult(interp, ObjNewList(nitems, itemObjs));
+    }
+    MemLifoPopMark(mark);
+    return res;
+}
+
 
 int Twapi_PdhGetFormattedCounterValue(
     Tcl_Interp *interp,
@@ -360,9 +435,50 @@ int Twapi_PdhBrowseCounters(Tcl_Interp *interp)
     PDH_BROWSE_DLG_CONFIG_W browse_dlg;
     PDH_STATUS pdh_status;
 
+
+
+
+    /* 
+     * This function is not documented because of the following bugs
+     * See http://helgeklein.com/blog/2009/10/found-my-first-bug-in-windows-7-apis-pdhbrowsecounters-requires-elevation/
+     *
+     * PDH.DLL on Vista & Win 7 is rife with bugs, this is just one of them
+     *
+     * You dont need elevation, you can just set this flag to false
+     * and it should work: bSingleCounterPerAdd = FALSE
+     * 
+     * Also, the flag bWildCardInstances must ALWAYS be set to TRUE,
+     * otherwise it causes crashes in many cases. The first one would
+     * be nice to be able to use (unelevated), but you will just have to
+     * ignore any secondary counters that are selected if a user
+     * selects more than one.
+     * 
+     * Other bugs: - You need to manually set the title, it no longer
+     * offers a default (NULL pointer).  - You cant set an initial
+     * performance counter to be highlighted.  - Sometimes the
+     * Instances wont show up on the bottom, depending on where you
+     * click on the top portion of the dialog box. This causes an
+     * invalid Counter path to be returned.  - PDH.DLL and 40 other
+     * DLLs are loaded, and STAY loaded once PdhBrowseCounters is
+     * called. Manually unloading the DLLs often causes crashes. This
+     * may or may not have something to do with .NET 4.0 being
+     * present.
+     * 
+     * Other general PDH bugs: - PdhExpandWildCardPath will not recognize
+     * any new Processes (and probably other instances for other object
+     * types), unless PDH.DLL is unloaded and reloaded. This also poses a
+     * problem if you use PdhBrowseCounters, as it increases the PDH.DLL
+     * load-count by 2. Forcing an un-load may be dangerous, but seems the
+     * only option.
+     * 
+     * My system: Win 7 x64, .Net 4.0, all updates as of 6/1/2011
+     */
+
     TwapiZeroMemory(&browse_dlg, sizeof(browse_dlg));
     browse_dlg.bIncludeInstanceIndex = 1;
     browse_dlg.bSingleCounterPerDialog = 1;
+    browse_dlg.bSingleCounterPerAdd = 1; /* See bug list above */
+    browse_dlg.bWildCardInstances = 1;   /* See bug list above */
     browse_dlg.cchReturnPathLength = 1000;
     /* Note we cannot use memlifo here as a callback is involved */
     browse_dlg.szReturnPathBuffer =
@@ -489,11 +605,12 @@ int Twapi_CallPdhObjCmd(TwapiInterpContext *ticP, Tcl_Interp *interp, int objc, 
         /* Free for all */
         switch (func) {
         case 1001:
+        case 1006:
             if (TwapiGetArgs(interp, objc-2, objv+2,
                              GETHANDLE(h), GETINT(dw),
                              ARGEND) != TCL_OK)
                 return TCL_ERROR;
-            return Twapi_PdhGetFormattedCounterValue(interp, h, dw);
+            return (func == 1001 ? Twapi_PdhGetFormattedCounterValue : Twapi_PdhGetFormattedCounterArray)(interp, h, dw);
         case 1002:
             if (TwapiGetArgs(interp, objc-2, objv+2,
                              GETHANDLE(h), ARGSKIP, GETINT(dw),
@@ -565,6 +682,7 @@ static int TwapiPdhInitCalls(Tcl_Interp *interp, TwapiInterpContext *ticP)
         DEFINE_ALIAS_CMD(PdhMakeCounterPath, 1003),
         DEFINE_ALIAS_CMD(PdhEnumObjectItems, 1004),
         DEFINE_ALIAS_CMD(PdhEnumObjects, 1005),
+        DEFINE_ALIAS_CMD(PdhGetFormattedCounterArray, 1006),
     };
 
     /* Create the underlying call dispatch commands */
