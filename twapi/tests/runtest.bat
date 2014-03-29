dfe17d9de4eff8c522ceba3fb42d568eafcd8951
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2014-03-29 15:52:36 +0530

    Fix recordarray getlist optimization when no args specified

--- twapi/tests/runtest.bat
+++ twapi/tests/runtest.bat
@@ -1,3 +1,4 @@
 set matchparam=%2
 if "x%matchparam%" == "x" set matchparam="*"
-tclsh86t %1.test -constraint userInteraction -verbose pe -match %matchparam% %3 %4 %5 %6 %7 %8 %9
+if "x%TCLSH%" == "x" set TCLSH=tclsh86t
+%TCLSH% %1.test -constraint userInteraction -verbose pe -match %matchparam% %3 %4 %5 %6 %7 %8 %9
